1bbe2c325a6f2c1fb612c3fa0f636b3679219a08
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-06-06 08:55:12 -0500

    create nameonly parser

---
+++ tests/test_parsers/test_nameonly.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from eveparser.parsers.nameonly import NameOnly
+
+
+class NameOnlyTestCase(TestCase):
+    def test_example_passes(self):
+        self.assertEqual(NameOnly.parse("Ragnarok"), ("Ragnarok", 1))
+
+    def test_spaced_name_passes(self):
+        self.assertEqual(NameOnly.parse("Siege Module II"), ("Siege Module II", 1))
