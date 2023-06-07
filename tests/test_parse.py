2a444b57ed4056350e42cc7d05fc628c4b1bfe52
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-06-07 07:22:58 -0500

    test parse returns type_id as well

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -6,9 +6,9 @@ from eveparse.errors import ParserError
 
 class ParseTestCase(TestCase):
     def test_all_examples_pass(self):
-        self.assertEqual(parse("Ragnarok 2"), ("Ragnarok", 2))
-        self.assertEqual(parse("2 Ragnarok"), ("Ragnarok", 2))
-        self.assertEqual(parse("Ragnarok"), ("Ragnarok", 1))
+        self.assertEqual(parse("Ragnarok 2"), (23773, "Ragnarok", 2))
+        self.assertEqual(parse("2 Ragnarok"), (23773, "Ragnarok", 2))
+        self.assertEqual(parse("Ragnarok"), (23773, "Ragnarok", 1))
 
     def test_invalid_name_fails(self):
         self.assertRaises(ParserError, parse, "string")
