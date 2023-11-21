a6f7d3e2fc2e752a36f008b27790bf190e5f585f
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-11-21 13:48:49 -0600

    Create main.py

---
+++ eveparse/main.py
@@ -0,0 +1,27 @@
+import exceptions
+
+parsers = [
+
+]
+
+
+def parse(text: str) -> dict:
+    items = {}
+    errors = []
+    for line in text.splitlines():
+        normalized_line = line.casefold()
+        for parser in parsers:
+            try:
+                type_dict = parser(normalized_line)
+            except exceptions.ParserException:
+                continue
+            else:
+                for type_id, quantity in type_dict.items():
+                    if type_id in items:
+                        items[type_id] += quantity
+                    else:
+                        items[type_id] = quantity
+                break
+        else:
+            errors.append(line)
+    return dict(items=items, errors=errors)
